6ec757673f272aae3ac6a752f2c945b68f6af646
Rixbeck <rix@neologik.hu>
2015-02-18 14:51:02 +0100

    Groups service handler

--- src/Service/Directory/GroupsService.php
+++ src/Service/Directory/GroupsService.php
@@ -1,16 +1,20 @@
 <?php
-namespace Bolt\Extension\Rixbeck\Gapps\Service;
+namespace Bolt\Extension\Rixbeck\Gapps\Service\Directory;
 
 use Bolt\Extension\Rixbeck\Gapps\Extension;
 use Bolt\Extension\Rixbeck\Gapps\RecordType;
 use Bolt\Application;
+use Bolt\Extension\Rixbeck\Gapps\Iterator\PagingGroupsIterator;
+use Bolt\Extension\Rixbeck\Gapps\Service\BaseService;
 
 class GroupsService extends BaseService
 {
 
+    public $groups;
+
     public function __construct(Application $app, $name)
     {
-        parent::__construct($app, $name, 'directory');
+        parent::__construct($app, $name, 'directory.groups');
     }
 
     public function initialize()
@@ -29,5 +33,24 @@ class GroupsService extends BaseService
     protected function createService($client)
     {
         return $this->service = new \Google_Service_Directory($client);
+    }
+
+    public function groupList($options = array())
+    {
+        $options['domain'] = $this->config['domain'];
+        $options = $this->prepareOptions($options);
+
+        return $this->groups = new PagingGroupsIterator($this->service->groups, $options);
+    }
+
+    /*
+     * (non-PHPdoc)
+     * @see \Bolt\Extension\Rixbeck\Gapps\Service\BaseService::createServiceName()
+     */
+    protected function createServiceName($section)
+    {
+        return array(
+            'admin.directory.group'
+        );
     }
 }
