02193fe4c60e4d452f42e5333bdfd0ccc0f24181
Rixbeck <rix@neologik.hu>
2015-02-18 14:50:02 +0100

    frontend/backend provider initiation decision comes in

--- src/Provider/CalendarServiceProvider.php
+++ src/Provider/CalendarServiceProvider.php
@@ -2,13 +2,15 @@
 namespace Bolt\Extension\Rixbeck\Gapps\Provider;
 
 use Bolt\Extension\Rixbeck\Gapps\Provider\BaseServiceProvider;
+use Bolt\Application;
 
 class CalendarServiceProvider extends BaseServiceProvider
 {
 
-    public function __construct($frontend)
+    public function __construct(Application $app)
     {
-        $this->sectionId = 'calendar';
+        $frontend = ($app['config']->getWhichEnd() === 'frontend');
         parent::__construct(($frontend) ? 'CalendarBase' : 'Calendar');
+        $this->sectionId = 'calendar';
     }
 }
