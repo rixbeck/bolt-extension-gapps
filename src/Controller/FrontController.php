8d1034f17dfff3e5db95860c8224d2cff98b3962
Rixbeck <rix@neologik.hu>
2015-02-26 23:12:27 +0100

    Controller for routing in calendar entry

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,16 +2,43 @@
 namespace Bolt\Extension\Rixbeck\Gapps\Controller;
 
 use Silex\ControllerProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Bolt\Extension\Rixbeck\Gapps\Extension;
 
-class FrontController implements ControllerProviderInterface
+class FrontController
 {
 
     /*
      * (non-PHPdoc)
      * @see \Silex\ControllerProviderInterface::connect()
      */
-    public function connect(Silex\Application $app)
+    public function connect(\Silex\Application $app)
     {
         // TODO: Auto-generated method stub
+    }
+
+    public static function event(Request $request, \Silex\Application $app, $calname, $eventid)
+    {
+        // $app['twig']->addGlobal('calendar', $calname);
+        // $app['twig']->addGlobal('eventid', $eventid);
+
+        /* @var $service \Bolt\Extension\Rixbeck\Gapps\Service\CalendarService */
+        $service = $app[Extension::getProviderId('calendar')][$calname];
+        $service->initialize();
+        $event = $service->getEvent($eventid);
+
+        $app['twig']->addGlobal('event', $event);
+        $app['twig']->addGlobal('calendar', $calname);
+        $template = 'calendareventrecord.twig';
+
+        return $app['twig']->render($template);
+    }
+
+    public static function events(Request $request, \Silex\Application $app, $calname)
+    {
+        $app['twig']->addGlobal('calendar', $calname);
+        $template = 'calendarlisting.twig';
+
+        return $app['twig']->render($template);
     }
 }
