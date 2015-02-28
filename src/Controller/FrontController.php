1ad2e4828ad91c6c8b129a510f350f46e71d9f65
Rixbeck <rix@neologik.hu>
2015-02-28 13:42:58 +0100

    Reciving template from routing config

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -17,11 +17,21 @@ class FrontController
         // TODO: Auto-generated method stub
     }
 
-    public static function event(Request $request, \Silex\Application $app, $calname, $eventid)
+    /**
+     * Route controller entry point. In routing.yml can define how Bolt can display a calendar event directly from GCalendar.
+     * Sample definition could be:
+     * event:
+     *   path: /event/{calname}/{eventid}
+     *   defaults: { _controller: 'Bolt\Extension\Rixbeck\Gapps\Controller\FrontController::event', template: 'calendareventrecord.twig' }
+     *
+     * @param Request $request
+     * @param \Silex\Application $app
+     * @param string $calname Name of calendar in config.yml
+     * @param string $eventid Id of an event in calendar
+     * @param string $template Template to be rendered
+     */
+    public static function event(Request $request, \Silex\Application $app, $calname, $eventid, $template = 'calendareventrecord.twig')
     {
-        // $app['twig']->addGlobal('calendar', $calname);
-        // $app['twig']->addGlobal('eventid', $eventid);
-
         /* @var $service \Bolt\Extension\Rixbeck\Gapps\Service\CalendarService */
         $service = $app[Extension::getProviderId('calendar')][$calname];
         $service->initialize();
@@ -29,15 +39,25 @@ class FrontController
 
         $app['twig']->addGlobal('event', $event);
         $app['twig']->addGlobal('calendar', $calname);
-        $template = 'calendareventrecord.twig';
 
         return $app['twig']->render($template);
     }
 
-    public static function events(Request $request, \Silex\Application $app, $calname)
+    /**
+     * Route controller entry point for lising events in GCalendar direcctly.
+     * Sample definition could be:
+     * events:
+     *   path: /event/{calname}
+     *   defaults: { _controller: 'Bolt\Extension\Rixbeck\Gapps\Controller\FrontController::events', template: 'calendarlisting.twig' }
+     *
+     * @param Request $request
+     * @param \Silex\Application $app
+     * @param string $calname Calendar name in config.yml
+     * @param string $template Template to berendered with
+     */
+    public static function events(Request $request, \Silex\Application $app, $calname, $template = 'calendarlisting.twig')
     {
         $app['twig']->addGlobal('calendar', $calname);
-        $template = 'calendarlisting.twig';
 
         return $app['twig']->render($template);
     }
